2054c4e0e6585d3a67076997161aad94eef7d2fd
EdSheranaway <edenshirin@gmail.com>
2022-09-20 17:16:50 -0500

    starting to modularize

--- client/components/ChoreContainer.jsx
+++ client/components/ChoreContainer.jsx
@@ -13,18 +13,24 @@ function ChoreContainer() {
   const [chores, setChores] = useState('');
   const [points, setPoints] = useState(0);
   const [priority, setPriority] = useState(0);
-  const [ refresh, setRefresh ] = useState(false);
+
+  //* here is a stateful component, put mapToDispatch stuff
+
+  const getData = () => {
+    fetch('/family/1')
+    .then(res => res.json())
+    .then((respdata) =>{
+    console.log('data',respdata);
+    setData(respdata)
+    console.log('this is data', data);
+    })
+    .catch((err) => {
+    })
+  }
 
   useEffect(() => {
-    fetch('/family/1')
-      .then(res => res.json())
-      .then((respdata) =>{
-      console.log('data',respdata);
-      setData(respdata)
-      })
-      .catch((err) => {
-      })
-  }, [refresh]);
+   getData();
+  }, []);
 
   const handleSubmit = (e) => {
     let newChore = e.target[0].value;
@@ -94,3 +100,75 @@ function ChoreContainer() {
 }
 
 export default ChoreContainer;
+//function ChoreContainer() {
+  // const [data, setData] = useState([
+  //   // { chores: 'sweep', points: 20, priority: 15 },
+  //   // { chores: 'brian', points: 20, priority: 15 },
+  //   // { chores: 'louis', points: 20, priority: 15 },
+  // ]);
+
+  // const [chores, setChores] = useState('');
+  // const [points, setPoints] = useState(0);
+  // const [priority, setPriority] = useState(0);
+// const [ refresh, setRefresh ] = useState(false);
+// useEffect(() => {
+//   fetch('/family/1')
+//     .then(res => res.json())
+//     .then((respdata) =>{
+//     console.log('data',respdata);
+//     setData(respdata)
+//     console.log('this is data', data);
+//     })
+//     .catch((err) => {
+//     })
+// }, [refresh]);
+
+// const handleSubmit = (e) => {
+//   let newChore = e.target[0].value;
+//   let newPoints = e.target[1].value;
+//   let newPriority = e.target[2].value;
+
+//   e.preventDefault();
+//   fetch('/family/1', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       chore: newChore,
+//       points: newPoints,
+//       priority: newPriority,
+//     }),
+//   })
+//     .then((data) => data.json())
+//     .catch((err) => console.log(err));
+//   const newRefresh = !refresh 
+//   setRefresh(newRefresh)
+//   setData([
+//     ...data,
+//     { chores: newChore, points: newPoints, priority: newPriority },
+//   ]);
+//   document.getElementById('itemInput').value = null;
+//   document.getElementById('numInput').value = null;
+//   document.getElementById('numInput2').value = null;
+// };
+// // console.log(chores, points, priority);
+// // console.log(data);
+
+// const handleDelete = (id) => {
+//   // e.preventDefault();
+//   const choreId = id;
+//   fetch('/individual/1', {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ id: choreId }),
+//   })
+//     .then((data) => {
+//       const newRefresh = !refresh 
+//       setRefresh(newRefresh)
+//       data.json()})
+//     .catch((err) => console.log(err));
+//   // setData(data.filter((item) => item._id !== id));
+// };
